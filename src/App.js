cece77a0813d1dcb7083d66bebb53ee685d9642d
David McDeavitt <davidmcdeavitt@192-168-1-8.tpgi.com.au>
2018-04-23 11:54:50 +1000

    building up

--- src/App.js
+++ src/App.js
@@ -2,24 +2,39 @@ import React, { Component } from 'react'
 import ListBooks from './ListBooks'
 
 
-const books = [
-  {
-    id: '123456',
-    name: 'Stormy Daniels',
-    shelf: '"wantToRead", "currentlyReading", "read"'
-  },
-  {
-    id: '111111',
-    name: 'Donald Trump',
-    shelf: '"currentlyReading'
-  },
-]
+
 
 class App extends Component {
+  state = {
+    books: [
+      {
+        id: '123456',
+        name: 'Stormy Daniels',
+        shelf: 'read'
+      },
+      {
+        id: '111111',
+        name: 'Donald Trump',
+        shelf: 'currentlyReading'
+      },
+    ]
+  }
+
+  changeShelf = (book) => {
+    this.setState((currentState) => ({
+      books: currentState.books.shelf.filter((c) => {
+        return c.id !== book.id
+      })
+    }))
+  }
+
   render() {
     return (
       <div>
-        <ListBooks books={books} />
+        <ListBooks
+          books={this.state.books}
+          onClickChangeShelf={this.changeShelf}
+        />
       </div>
     )
   }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class ListBooks extends Component {
-  render () {
-    console.log('Props', this.props)
-    return (
-      <ol className='books-list'>
-        {this.props.books.map((book) => (
-          <li key={book.id}>
-            {book.name}
-          </li>
-        ))}
-      </ol>
+  static PropTypes = {
+    contacts: PropTypes.array.isRequired,
+    onClickChangeShelf: PropTypes.func.isRequired,
+  }
+  state = {
+    query: ''
+  }
+  updateQuery = (query) => {
+    this.setState(() => ({
+      query: query.trim()
+    }))
+  }
+  render() {
+    return(
+      <div className='list-books'>
+        {JSON.stringify(this.state)}
+        <div className='search-books-top'>
+          <input
+            className='search-books'
+            type='text'
+            placeholder='Search Books'
+            value={
+            this.state.query
+            }
+            onChange={(event) => this.updateQuery(event.target.value)}
+          />
+        </div>
+        <ol className='books-list'>
+          {this.props.books.map((book) => (
+            <li key={book.id} className='book-list-item'>
+              <div className='book-details'>
+                <p>{book.name}</p>
+                <p>{book.shelf}</p>
+              </div>
+              <button onClick={() => this.props.onClickChangeShelf(book.shelf)}className='contact-dropdown'>
+                Change Shelf
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+
     )
   }
+
 }
+
+
+
+
 
 export default ListBooks
